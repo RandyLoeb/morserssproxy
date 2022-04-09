653830a37e8b90243cca7f9c6b9f2fc8b58b7c57
Randy Loeb <githubrandyloeb@randonloeb.com>
2022-04-08 22:17:27 -0400

    eslint

--- proxy.js
+++ proxy.js
@@ -1,14 +1,15 @@
 // https://github.com/Rob--W/cors-anywhere
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '127.0.0.1';
+const host = process.env.HOST || '192.168.0.135'
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8085;
+const port = process.env.PORT || 8085
 
-var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
+const corsProxy = require('cors-anywhere');
+
+corsProxy.createServer({
+  originWhitelist: [], // Allow all origins
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function () {
+  console.log('Running CORS Anywhere on ' + host + ':' + port)
+})
